fc9c71de041de3b0966b883497edcc12e7c6da8d
= <aquinas>
2026-03-06 05:52:43 +0300

    Убрал header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,6 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
           disableTransitionOnChange
         >
           <div className="relative flex min-h-screen flex-col">
-            <SiteHeader />
             <main className="flex-1">{children}</main>
           </div>
           <TailwindIndicator />
